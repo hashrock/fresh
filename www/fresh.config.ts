4363585046b3c92be3ee1ab1be9866d1b8d2c43f
hashrock <hashrock@users.noreply.github.com>
2023-12-25 19:10:00 +0900

    debug

--- www/fresh.config.ts
+++ www/fresh.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 import storiesPlugin from "https://deno.land/x/fresh_stories@0.0.10/stories-plugin.ts";
 
+console.log("import.meta.url", import.meta.url);
+
 export default defineConfig({
   build: {
     target: "safari12",
